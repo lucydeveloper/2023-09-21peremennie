089b34fcc15183eb32896561503f9a7092ee5fd1
lucydeveloper <ludmilla.girina@gmail.com>
2024-09-08 22:37:28 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+completed_activities_count = 12
+print(completed_activities_count)
+hours_count = 1.5
+print(hours_count)
+course_name = "Python"
+print(course_name)
+time_per_task = (hours_count / completed_activities_count)
+print(time_per_task)
+
+# Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
+print("Курс: " + course_name + ", всего задач:" + str(completed_activities_count) + ", затрачено часов: " + str(hours_count) + ", среднее время выполнения " + str(time_per_task) + " часа.")
+
